94af418d6ee3c0c329d7d922069c74d626cb7883
darenprince <161107418+darenprince@users.noreply.github.com>
2025-08-06 23:48:42 -0500

    feat: centralize supabase client

--- js/supabaseClient.js
+++
@@ -1,4 +0,0 @@
-import { SUPABASE_URL, SUPABASE_KEY } from './env.js';
-
-export const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-window.supabaseClient = supabaseClient;
