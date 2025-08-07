852aa3f473dd90be3f031552b8c45d5975d797d2
darenprince <161107418+darenprince@users.noreply.github.com>
2025-08-07 10:23:47 -0500

    Warn instead of throwing in deployment env script

--- scripts/generate-env.js
+++ scripts/generate-env.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 const path = require('path');
 
 const {
-  SUPABASE_DATABASE_URL,
-  SUPABASE_ANON_KEY,
+  SUPABASE_DATABASE_URL = '',
+  SUPABASE_ANON_KEY = '',
   SUPABASE_SERVICE_ROLE_KEY = '',
   SUPABASE_JWT_SECRET = '',
 } = process.env;
 
 if (!SUPABASE_DATABASE_URL || !SUPABASE_ANON_KEY) {
-  throw new Error('SUPABASE_DATABASE_URL and SUPABASE_ANON_KEY must be set');
+  console.warn('SUPABASE_DATABASE_URL and SUPABASE_ANON_KEY must be set');
 }
 
 const content = `export const SUPABASE_DATABASE_URL = '${SUPABASE_DATABASE_URL}';\nexport const SUPABASE_ANON_KEY = '${SUPABASE_ANON_KEY}';\nexport const SUPABASE_SERVICE_ROLE_KEY = '${SUPABASE_SERVICE_ROLE_KEY}';\nexport const SUPABASE_JWT_SECRET = '${SUPABASE_JWT_SECRET}';\n`;
