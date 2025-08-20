f6e09c7f1fd08cd0306ffa06262203596fb2e8cf
darenprince <161107418+darenprince@users.noreply.github.com>
2025-08-20 00:02:25 -0500

    Implement book rail view handlers

--- js/book-rail.js
+++ js/book-rail.js
@@ -1,9 +1,15 @@
 const rail = document.querySelector('.book-rail');
 
 export function onZoom() {}
-export function onViewFront() {}
-export function onViewBack() {}
-export function onToggleSpin360() {}
+export function onViewFront() {
+  setActiveTool('front');
+}
+export function onViewBack() {
+  setActiveTool('back');
+}
+export function onToggleSpin360() {
+  setActiveTool('spin360');
+}
 export function onBuyClick() {}
 
 export function setActiveTool(name) {
